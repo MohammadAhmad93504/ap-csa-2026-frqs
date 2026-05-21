c4c930ca6d46dcc206a17d973b3292ef75884197
MohammadAhmad93504 <mohammada169@nycstudents.net>
2026-05-20 21:11:37 -0400

    Delete question-1/PartA.java

--- question-1/PartA.java
+++
@@ -1,8 +0,0 @@
-public Account(String requestedName) {
-      username = requestedName;
-      int i = 1;
-      while (!isAvailable(username)) {
-          username = requestedName + i;
-          i++;
-      }
-}
