1e0b6fb1dd78089d46e4a6f60b93a11848d57136
MohammadAhmad93504 <mohammada169@nycstudents.net>
2026-05-22 18:44:35 -0400

    Add method to calculate points for a row

--- question-4/Solution.java
+++ question-4/Solution.java
@@ -1,1 +1,15 @@
-
+public int getPointsForRow(int targetRow) {
+    boolean streak = true; 
+    int total = 0; 
+    for (int c = 0; c < board[0].length; c++){ 
+        total+= board[targetRow][c].getPoints(); 
+        String color = board[targetRow][0].getColor(); 
+        if (!(board[targetRow][c].getColor().equals(color))){
+            streak = false; 
+        }
+    }
+    if (word) {
+        total*=2; 
+    }
+    return total; 
+}
