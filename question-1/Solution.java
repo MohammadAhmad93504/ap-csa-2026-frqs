87e95d8b70c143001531b41f510ce95aec973e64
MohammadAhmad93504 <mohammada169@nycstudents.net>
2026-05-22 17:44:34 -0400

    Implement username availability check in constructor
    Add constructor to ensure unique username.

--- question-1/Solution.java
+++ question-1/Solution.java
@@ -1,1 +1,8 @@
-
+public Account(String requestedName) {
+      username = requestedName;
+      int i = 1;
+      while (!isAvailable(username)) {
+          username = requestedName + i;
+          i++;
+      }
+}
