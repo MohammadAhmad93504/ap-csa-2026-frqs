e0afeab41a3c5882f0e8b6a458f0eccb87a6ea92
MohammadAhmad93504 <mohammada169@nycstudents.net>
2026-05-22 18:37:07 -0400

    Add method to count students with more history absences

--- question-3/Solution.java
+++ question-3/Solution.java
@@ -1,1 +1,21 @@
+public int countGreaterHistoryAbsences() { 
+    int totalMatchingStudents = 0;
+    
+    for (CourseRecord historyRecord : historyList) {
+        String historyID = historyRecord.getStudentID(); 
+        int historyAbs = historyRecord.getAbsences(); 
 
+        for (CourseRecord mathRecord : mathList) {
+            String mathID = mathRecord.getStudentID();
+            
+            if (mathID.equals(historyID)) {
+                int mathAbs = mathRecord.getAbsences();
+                
+                if (historyAbs > mathAbs) {
+                    totalMatchingStudents++; 
+                }
+            }
+        }       
+    }
+    return totalMatchingStudents;
+}
