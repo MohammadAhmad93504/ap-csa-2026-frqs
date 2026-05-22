45fa9ee419e5b2ca1c057ff7dfbb6215ff99106f
MohammadAhmad93504 <mohammada169@nycstudents.net>
2026-05-22 18:33:59 -0400

    Bottle class
    Add Bottle class with volume management

--- question-2/Solution.java
+++ question-2/Solution.java
@@ -1,1 +1,20 @@
+public class Bottle {
+    private double maxVolume; 
+    private double currentVolume; 
 
+    public Bottle(double totalCapacity) {
+        maxVolume = totalCapacity; 
+        currentVolume = maxVolume; 
+    }
+
+    public double updateAmount(double amountChanged) {
+        currentVolume = currentVolume - amountChanged; 
+        
+        double quarterMark = maxVolume * 0.25;
+        if (currentVolume < quarterMark) {
+            currentVolume = maxVolume; 
+        }
+        
+        return currentVolume; 
+    }
+}
